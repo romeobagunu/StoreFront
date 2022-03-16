8227f0654460bea8bac81bc5c93ff18482687df3
Romeo Bagunu <romeo.bagunu@outlook.com>
2022-03-15 20:28:04 -0500

    Bundled template CSS and JS in the Layout.

--- StoreFront.UI.MVC/App_Start/BundleConfig.cs
+++ StoreFront.UI.MVC/App_Start/BundleConfig.cs
@@ -7,8 +7,8 @@ namespace StoreFront.UI.MVC
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        //"~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -18,13 +18,31 @@ namespace StoreFront.UI.MVC
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      //"~/Scripts/bootstrap.js",
+                      //"~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+                      //"~/Content/bootstrap.css",
+                      //"~/Content/site.css"));
+
+            bundles.Add(new Bundle("~/bundles/scripts-template").Include(
+                "~/Content/vendor/bootstrap/js/bootstrap.bundle.min.js",
+                "~/Content/vendor/waypoints/lib/noframework.waypoints.js",
+                "~/Content/vendor/swiper/swiper-bundle.min.js",
+                "~/Content/vendor/choices.js/public/assets/scripts/choices.js",
+                "~/Scripts/theme.js"
+                ));
+
+            bundles.Add(new StyleBundle("~/Content/css-template").Include(
+                "~/Content/vendor/swiper/swiper-bundle.min.css",
+                "~/Content/vendor/choices.js/public/assets/styles/choices.css",
+                "~/Content/css/style.default.css",
+                "~/Content/css/custom.css"
+                ));
+
+            
+
         }
     }
 }
