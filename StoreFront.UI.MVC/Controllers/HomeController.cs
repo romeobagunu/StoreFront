acab3c96a8da20a9657a34c77d2206a07e69c458
Romeo Bagunu <romeo.bagunu@outlook.com>
2022-03-18 08:35:28 -0500

    Added Contact functionality, including ViewModel, Action, View, and Confirmation View.

--- StoreFront.UI.MVC/Controllers/HomeController.cs
+++ StoreFront.UI.MVC/Controllers/HomeController.cs
@@ -1,4 +1,12 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StoreFront.UI.MVC.Models;//Added for access to ContactViewModel.
+using System.Net.Mail;//Added for access to MailMessage.
+using System.Configuration;//Added for access to the config.
+using System.Net;//Added for access to NetworkCredential.
 
 namespace StoreFront.UI.MVC.Controllers
 {
@@ -22,9 +30,53 @@ namespace StoreFront.UI.MVC.Controllers
         [HttpGet]
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cvm);
+            }
+
+            string msg = $"{cvm.Name} has sent you the following message:<br />" +
+                $"Subject: <strong>{cvm.Subject}</strong><br />" +
+                $"{cvm.Message}" +
+                $"Please reply to {cvm.Email} at your earliest convenience.";
+
+            MailMessage mm = new MailMessage(
+                ConfigurationManager.AppSettings["EmailUser"].ToString(),
+                ConfigurationManager.AppSettings["EmailTo"].ToString(),
+                cvm.Subject,
+                msg);
+
+            mm.IsBodyHtml = true;
+            mm.Priority = MailPriority.High;
+            mm.ReplyToList.Add(cvm.Email);
+
+            SmtpClient client = new SmtpClient(ConfigurationManager.AppSettings["EmailClient"].ToString());
+
+            client.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["EmailUser"].ToString(), ConfigurationManager.AppSettings["EmailPass"].ToString());
+
+            try
+            {
+                //Send email
+                client.Send(mm);
+            }
+            catch (Exception ex)
+            {
+                //Log error in the ViewBag
+                ViewBag.CustomerMessage = $"We're sorry. Your request could not be completed at this time." +
+                    $"Please try again later! <br />" +
+                    $"Error Message:<br />" +
+                    $"{ex.StackTrace}";
+                return View(cvm);
+            }
+
+            return View("EmailConfirmation", cvm);
+        }
+
     }
 }
---
+++ StoreFront.UI.MVC/Models/ContactViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace StoreFront.UI.MVC.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "*Your name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "*Your email is required.")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "*A subject is required.")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "*A message is required.")]
+        [UIHint("MultilineText")]
+        public string Message { get; set; }
+    }
+}
