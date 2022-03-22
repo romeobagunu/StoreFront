224fb0078f9f1e313090142916745a6a8ccd62c1
Romeo Bagunu <romeo.bagunu@outlook.com>
2022-03-21 21:43:23 -0500

    Added a custom 404 error page.

---
+++ StoreFront.UI.MVC/Controllers/CustomErrorsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StoreFront.UI.MVC.Controllers
+{
+    public class CustomErrorsController : Controller
+    {
+        // GET: CustomErrors
+        public ActionResult Error404()
+        {
+            return View();
+        }
+    }
+}
